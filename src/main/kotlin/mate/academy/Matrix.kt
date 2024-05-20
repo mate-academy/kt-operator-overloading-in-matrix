b01bf6376e67954ddaade3d327332479e09cd202
Сидоркін Андрій Володимирович <sydorkin-av@ukrposhta.ua>
2024-05-20 10:39:14 +0300

    Tests are passed

--- src/main/kotlin/mate/academy/Matrix.kt
+++ src/main/kotlin/mate/academy/Matrix.kt
@@ -8,7 +8,25 @@ class Matrix(private val rows: Int, private val cols: Int) {
         data[row][col] = value
     }
 
-    // TODO: implement overloading operators for matrix addition and subtraction
+    operator fun plus(other: Matrix): Matrix {
+        val result = Matrix(this.rows, this.cols)
+        data.forEachIndexed { xIndex, rows ->
+            rows.forEachIndexed { yIndex, firstVal ->
+                result[xIndex, yIndex] = firstVal + other[xIndex, yIndex]
+            }
+        }
+        return result
+    }
+
+    operator fun minus(other: Matrix): Matrix {
+        val result = Matrix(this.rows, this.cols)
+        data.forEachIndexed { xIndex, rows ->
+            rows.forEachIndexed { yIndex, firstVal ->
+                result[xIndex, yIndex] = firstVal - other[xIndex, yIndex]
+            }
+        }
+        return result
+    }
 
     override fun toString(): String {
         return data.joinToString(separator = "\n") { row -> row.joinToString(" ") }
