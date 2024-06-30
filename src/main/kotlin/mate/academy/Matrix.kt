996663ec6f69456636f76c6dc27bbedf4deb701a
serhiibiria <serhiiburia@gmail.com>
2024-06-30 10:41:52 +0300

    added solution

--- src/main/kotlin/mate/academy/Matrix.kt
+++ src/main/kotlin/mate/academy/Matrix.kt
@@ -8,7 +8,33 @@ class Matrix(private val rows: Int, private val cols: Int) {
         data[row][col] = value
     }
 
-    // TODO: implement overloading operators for matrix addition and subtraction
+    operator fun plus(other: Matrix): Matrix {
+        require(this.rows == other.rows && this.cols == other.cols) {
+            "Matrices must have the same dimensions for addition"
+        }
+
+        val result = Matrix(this.rows, this.cols)
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                result[i, j] = this[i, j] + other[i, j]
+            }
+        }
+        return result
+    }
+
+    operator fun minus(other: Matrix): Matrix {
+        require(this.rows == other.rows && this.cols == other.cols) {
+            "Matrices must have the same dimensions for subtraction"
+        }
+
+        val result = Matrix(this.rows, this.cols)
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                result[i, j] = this[i, j] - other[i, j]
+            }
+        }
+        return result
+    }
 
     override fun toString(): String {
         return data.joinToString(separator = "\n") { row -> row.joinToString(" ") }
